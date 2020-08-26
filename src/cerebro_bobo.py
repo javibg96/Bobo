f60992cde46b5ee6ef6bc7fb9f7f82dcd89f6b3b
javibg96 <blascogarcia.javier@outlook.com>
2020-08-26 11:04:05 +0200

    tkinter working from its own script

---
+++ src/cerebro_bobo.py
@@ -0,0 +1,13 @@
+import src.interfaz.interfaz_handler as interfaz
+
+class Cerebro:
+
+    def __init__(self):
+
+
+    def main_interf(self):
+        interfaz.Interfaz()
+
+
+if __name__ == "__main__":
+    Cerebro().funciona()
