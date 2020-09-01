f33fc96a6f792bb17e0084fd168de1fe7737020c
javibg96 <blascogarcia.javier@outlook.com>
2020-09-01 12:34:50 +0200

    stt and tts added and working

--- src/cerebro_bobo.py
+++ src/cerebro_bobo.py
@@ -1,12 +1,25 @@
 import src.interfaz.interfaz_handler as interfaz
+import src.voz.stt_input_understanding as stt
+import src.voz.tts_output as tts
+
 
 class Cerebro:
 
     def __init__(self):
+        self.voz = tts.Voz()
 
+    def funciona(self):
+        texto = stt.stt_input()
+        while texto == "UnknownValueError":
+            self.voz.habla("Disculpe señor, no he logrado entenderle, ¿Puede repetirmelo?")
+            texto = stt.stt_input()
 
-    def main_interf(self):
-        interfaz.Interfaz()
+        print(texto)
+        self.voz.habla("Encantado señor, ¿que quiere que haga?")
+        texto2 = stt.stt_input()
+        print(texto2)
+        if "Spotify" in texto2:
+            self.voz.habla("Iniciando Spotify...")
 
 
 if __name__ == "__main__":
---
+++ src/voz/tts_output.py
@@ -0,0 +1,20 @@
+import pyttsx3 as p
+
+
+class Voz:
+    def __init__(self):
+        self.engine = p.init()
+        voices = self.engine.getProperty("voices")
+        self.engine.setProperty("voice", voices[2].id)
+        rate = self.engine.getProperty('rate')  # getting details of current speaking rate
+        self.engine.setProperty('rate', 150)  # setting up new voice rate
+        self.engine.say("hola, soy Bobo")
+        self.engine.runAndWait()
+
+
+    def habla(self, frase):
+
+        self.engine.say(frase)
+        self.engine.runAndWait()
+
+
